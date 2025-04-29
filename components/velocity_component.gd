2a3af56ec5cf352887cff7dccae2a90a6d6d9a0f
Igonorant <igolino3@hotmail.com>
2025-04-29 20:40:56 -0300

    Better organize code and improve player class

--- components/velocity_component.gd
+++ components/velocity_component.gd
@@ -1,42 +1,50 @@
-extends Node
 class_name VelocityComponent
+extends Node
 
 @export var velocity_limit : float = 300.0 # maximum velocity in pixels/sec
 @export var acceleration : float = 1500.0 # pixels/sec^2
-@export var dynamic_friction : float = 3.0 # decceleration in %/sec (1 means 100%/s)
-@export var static_friction : float = 300.0 # decceleration in pixels/sec^2
+@export var dynamic_friction : float = 3.0 # deceleration in %/sec (1 means 100%/s)
+@export var static_friction : float = 300.0 # deceleration in pixels/sec^2
+
 var m_velocity : Vector2
 var m_direction : Vector2
 
+
 func _ready() -> void:
-	m_velocity = Vector2.ZERO
-	m_direction = Vector2.ZERO
-	assert(acceleration > static_friction, "Friction should not be greater than acceleration, it doesn't make sense.")
+    m_velocity = Vector2.ZERO
+    m_direction = Vector2.ZERO
+    assert(acceleration > static_friction, "Friction should not be greater than acceleration—it doesn't make sense.")
+
 
 func _process(delta: float) -> void:
-	# Update velocity based on friction for X and Y components
-	m_velocity.x = _add_friction(m_velocity.x, delta)
-	m_velocity.y = _add_friction(m_velocity.y, delta)
-	
-	# If there is any direction, accelerate
-	if (!m_direction.is_zero_approx()):
-		m_velocity += m_direction.normalized() * (acceleration * delta)
-		m_velocity = m_velocity.clampf(-velocity_limit, velocity_limit)
+    # Apply friction to both components
+    m_velocity = Vector2(_add_friction(m_velocity.x, delta), _add_friction(m_velocity.y, delta))
 
-# Add friction based on current velocity.
+    # Accelerate if there is a direction input
+    if !m_direction.is_zero_approx():
+        m_velocity += m_direction.normalized() * (acceleration * delta)
+
+    # Clamp velocity within limits
+    m_velocity = m_velocity.limit_length(velocity_limit)
+
+
 func _add_friction(velocity_component: float, delta: float) -> float:
-	var static_friction_delta: float = static_friction * delta
-	if abs(velocity_component) < static_friction_delta:
-		return 0.0
-	# Apply dynamic friction
-	velocity_component -= velocity_component * dynamic_friction * delta
-	# Apply static friction in the correct direction
-	velocity_component -= sign(velocity_component) * static_friction_delta
-	return velocity_component
+    var deceleration: float = static_friction * delta
+
+    # Avoids oscilations in velocity when deceleration would invert velocity sign
+    if abs(velocity_component) < deceleration:
+        return 0.0
+
+    # Apply dynamic and static friction
+    velocity_component *= (1.0 - dynamic_friction * delta)
+    velocity_component -= sign(velocity_component) * deceleration
+
+    return velocity_component
 
 
 func set_direction(direction: Vector2) -> void:
-	m_direction = direction
+    m_direction = direction
+
 
 func get_velocity() -> Vector2:
-	return m_velocity
+    return m_velocity
--- player.gd
+++ player.gd
@@ -1,32 +1,46 @@
+class_name Player
 extends CharacterBody2D
+
+@onready var c_velocity : VelocityComponent = $VelocityComponent
+@onready var n_sprite : Sprite2D = $Sprite2D
 
 var m_screen_size # Size of the game window.
 var m_flip_h : bool # Flip the player horizontally.
-var c_velocity : VelocityComponent
+var m_player_input_direction : Vector2 # Player inputs direction
 
-func _ready():
-	m_screen_size = get_viewport_rect().size
-	c_velocity = $VelocityComponent
-	m_flip_h = false
-	
-func _process(delta):
-	var direction : Vector2 = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-		m_flip_h = true
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-		m_flip_h = false
-	if Input.is_action_pressed("move_down"):
-		direction.y += 1
-	if Input.is_action_pressed("move_up"):
-		direction.y -= 1
-	
-	c_velocity.set_direction(direction)
-	
-	var l_velocity : Vector2 = c_velocity.get_velocity()
-	
-	$Sprite2D.flip_h = m_flip_h
 
-	position += l_velocity * delta
-	position = position.clamp(Vector2.ZERO, m_screen_size)
+func _ready() -> void:
+    m_screen_size = get_viewport_rect().size
+    m_flip_h = false
+    m_player_input_direction = Vector2.ZERO
+
+    
+func _process(_delta: float) -> void:
+    _calculate_direction()
+    _move()
+    _update_sprite_direction()
+
+   
+func _calculate_direction() -> void:
+    m_player_input_direction = Vector2.ZERO
+    if Input.is_action_pressed("move_right"):
+        m_player_input_direction.x += 1
+        m_flip_h = true
+    if Input.is_action_pressed("move_left"):
+        m_player_input_direction.x -= 1
+        m_flip_h = false
+    if Input.is_action_pressed("move_down"):
+        m_player_input_direction.y += 1
+    if Input.is_action_pressed("move_up"):
+        m_player_input_direction.y -= 1
+
+
+func _move() -> void:
+    c_velocity.set_direction(m_player_input_direction)
+    velocity = c_velocity.get_velocity()
+    move_and_slide()
+    position = position.clamp(Vector2.ZERO, m_screen_size)
+
+
+func _update_sprite_direction() -> void:
+    n_sprite.flip_h = m_flip_h
