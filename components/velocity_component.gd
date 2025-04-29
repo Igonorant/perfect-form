e31b961455293c5239cbaf12f3c3fc10a2bdbdf2
Igonorant <igolino3@hotmail.com>
2025-04-29 20:10:34 -0300

    Updating friction system of velocity component

--- components/velocity_component.gd
+++ components/velocity_component.gd
@@ -1,22 +1,22 @@
 extends Node
 class_name VelocityComponent
 
-@export var velocity_limit : float = 300 # maximum velocity in pixels/sec
-@export var acceleration : float = 1500 # pixels/sec^2
-@export var friction : float = 0.05 # % of velocity
+@export var velocity_limit : float = 300.0 # maximum velocity in pixels/sec
+@export var acceleration : float = 1500.0 # pixels/sec^2
+@export var dynamic_friction : float = 3.0 # decceleration in %/sec (1 means 100%/s)
+@export var static_friction : float = 300.0 # decceleration in pixels/sec^2
 var m_velocity : Vector2
 var m_direction : Vector2
-var m_min_velocity : float
 
 func _ready() -> void:
 	m_velocity = Vector2.ZERO
 	m_direction = Vector2.ZERO
-	m_min_velocity = 0.02 * velocity_limit
+	assert(acceleration > static_friction, "Friction should not be greater than acceleration, it doesn't make sense.")
 
 func _process(delta: float) -> void:
 	# Update velocity based on friction for X and Y components
-	m_velocity.x = _add_friction(m_velocity.x)
-	m_velocity.y = _add_friction(m_velocity.y)
+	m_velocity.x = _add_friction(m_velocity.x, delta)
+	m_velocity.y = _add_friction(m_velocity.y, delta)
 	
 	# If there is any direction, accelerate
 	if (!m_direction.is_zero_approx()):
@@ -24,13 +24,16 @@ func _process(delta: float) -> void:
 		m_velocity = m_velocity.clampf(-velocity_limit, velocity_limit)
 
 # Add friction based on current velocity.
-# If it is less than 2% of the velocity limit, make it 0 to avoid going closer and closer to 0
-func _add_friction(velocity_component: float) -> float:
-	if (abs(velocity_component) < m_min_velocity):
+func _add_friction(velocity_component: float, delta: float) -> float:
+	var static_friction_delta: float = static_friction * delta
+	if abs(velocity_component) < static_friction_delta:
 		return 0.0
-	else:
-		velocity_component -= velocity_component * friction
-		return velocity_component
+	# Apply dynamic friction
+	velocity_component -= velocity_component * dynamic_friction * delta
+	# Apply static friction in the correct direction
+	velocity_component -= sign(velocity_component) * static_friction_delta
+	return velocity_component
+
 
 func set_direction(direction: Vector2) -> void:
 	m_direction = direction
