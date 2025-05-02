32e210e5bf3bfb3121570f08f1d1549997c7015d
Igonorant <igolino3@hotmail.com>
2025-05-02 20:08:47 -0300

    Updated projectile zap asset

--- traits/projectile_zap.gd
+++ traits/projectile_zap.gd
@@ -8,3 +8,4 @@ func _on_change_direction_timer_timeout() -> void:
     var new_dir = c_velocity.m_direction + Vector2(randf_range(max_variation, -max_variation), randf_range(max_variation, -max_variation))
     new_dir = new_dir.normalized()
     c_velocity.set_direction(new_dir)
+    rotation = new_dir.angle()
