605a74c0e9662c194ca0e4fe7df08033ae1b692d
Igonorant <igolino3@hotmail.com>
2025-05-02 21:30:05 -0300

    Remove debug messages from projectile burst

--- traits/projectile_burst.gd
+++ traits/projectile_burst.gd
@@ -1,41 +1,32 @@
-
 class_name ProjectileBurst
 extends Projectile
 
 @export var max_variation: float
 
-enum State { STARTING, RUNNING, EXPLODING }
+enum State {STARTING, RUNNING, EXPLODING}
 var state: State = State.STARTING
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
-    super()
-    sprite.animation = "start"
+    super ()
     sprite.play("start")
     sprite.animation_finished.connect(_on_animation_finished)
 
 func _on_animation_finished():
-    print("chamou callback")
-
     match state:
         State.STARTING:
             state = State.RUNNING
-            sprite.animation = "going"
-            sprite.play()
+            sprite.play("going")
         State.EXPLODING:
-            print("someeee")
-            queue_free()  # Or emit signal / pool it
+            queue_free() # Or emit signal / pool it
 
 func explode():
     if state != State.EXPLODING:
         state = State.EXPLODING
-        sprite.animation = "hit"
-        print("ta explodindo")
-        sprite.play()
+        sprite.play("hit")
 
 func _on_hurt_box_component_hurt_body(body: Node2D, _damage: float) -> void:
     if (body.is_in_group("enemies")):
         body.queue_free()
         explode()
-
