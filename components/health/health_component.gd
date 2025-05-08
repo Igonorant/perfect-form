0b4646ec71db4f3e9cf985c2d85b8c17bcdcb270
Igonorant <igolino3@hotmail.com>
2025-05-08 10:17:02 -0300

    Updating health component to a Node2D so bar can be positioned in the scene itself

--- components/health/health_component.gd
+++ components/health/health_component.gd
@@ -1,6 +1,6 @@
 @icon("res://assets/icons/health.png")
 class_name HealthComponent
-extends Node
+extends Node2D
 
 @export var max_health: float = 100.0
 @export var add_health_bar: bool = true
@@ -22,16 +22,6 @@ func _configure_health_bar() -> void:
     _health_bar.min_value = 0.0
     _health_bar.max_value = max_health
     _health_bar.value = _current_health
-
-    _health_bar.set_size(Vector2(48, 8))
-    _health_bar.set_position(_get_health_bar_position())
-
-func _physics_process(_delta: float) -> void:
-    if _health_bar:
-        _health_bar.set_position(_get_health_bar_position())
-
-func _get_health_bar_position() -> Vector2:
-    return _parent.get_global_position() + Vector2(-24, -36)
 
 func deal_damage(damage: float) -> void:
     _current_health -= damage
